e5c140a45bbbf1d85ec0ca5d1a9dd83ee3b99cb2
TisPO8 <9853891+tispo8@user.noreply.gitee.com>
2023-05-13 16:32:45 +0800

    test(custom): 工程化基本架构测试
    基本已经完成了

--- .cz-config.js
+++ .cz-config.js
@@ -1,28 +1,27 @@
 module.exports = {
   // 可选类型
   types: [
-    { value: 'feat', name: '🚩 feat:     新功能' },
+    { value: 'feat', name: '✨ feat:     新功能' },
+    { value: 'test', name: '✅ test:     增加测试' },
     { value: 'fix', name: '🐛 fix:      修复' },
     { value: 'docs', name: '📝 docs:     文档变更' },
     { value: 'style', name: '💄 style:    代码格式(不影响代码运行的变动)' },
     { value: 'refactor', name: '♻️  refactor: 重构(既不是增加feature，也不是修复bug)'},
     { value: 'perf', name: '⚡️ perf:     性能优化' },
-    { value: 'test', name: '👷 test:     增加测试' },
     { value: 'chore', name: '🔧 chore:    改变构建流程、或者增加依赖库、工具等' },
     { value: 'revert', name: '⏪️ revert:   回退' },
-    { value: 'build', name: '🔥 build:    打包发布版本' }
   ],
   // 消息步骤
   messages: {
     type: '请选择提交类型(必填):',
     customScope: '请输入修改范围(可选):',
     subject: '请简要描述提交(必填):',
-    body: '请输入详细描述(可选):',
+    body: '请输入详细描述(必填):',
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
   // 跳过问题
-  skipQuestions: ['customScope','body', 'footer'],
+  skipQuestions: ['customScope', 'footer'],
   // subject文字长度限制默认是72
   subjectLimit: 72
 }
